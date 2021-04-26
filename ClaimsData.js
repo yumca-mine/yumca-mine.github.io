3a1e32a83e2edce86c4f647880dd16e255f1a250
Fabian Röling <fabianroeling@googlemail.com>
2021-04-26 21:59:26 +0200

    Update ClaimsData.js

--- ClaimsData.js
+++ ClaimsData.js
@@ -97,4 +97,5 @@ drawClaim("@limebot claim -4375 675 -4625 925 Alpine's Farms",160);
 drawClaim("@limebot claim 2536 -2551 2572 -2526 Mendax's Rave Ravine",161);
 drawClaim("@limebot claim 2400 -600 2700 -300 Non Empty Land",162);
 drawClaim("@limebot claim 4800 2200 4850 2260 SPAntarctica",163);
+drawClaim("@limebot claim 6489 -4706 6922 -5090 Cookieteers' Raceway",164);
 }

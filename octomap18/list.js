ba0790a1460baa940bd0ff123349b533131220f0
octojen <octojenn@users.noreply.github.com>
2022-01-09 02:05:11 +0000

    octomap: New 1.18  tiles!

--- octomap18/list.js
+++ octomap18/list.js
@@ -1,1 +1,16 @@
 octo18list.push("0,0");
+octo18list.push("1,0");
+octo18list.push("1,1");
+octo18list.push("-1,1");
+octo18list.push("-1.-1");
+octo18list.push("2,0");
+octo18list.push("-2,0");
+octo18list.push("-2,1");
+octo18list.push("-2,-1");
+octo18list.push("2,-2");
+octo18list.push("3,0");
+octo18list.push("-3,1");
+octo18list.push("4,0");
+octo18list.push("4,1");
+octo18list.push("5,1");
+octo18list.push("6,1");

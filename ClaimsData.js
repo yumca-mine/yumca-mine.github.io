b7f9461ce65735d9f1c348aee89d149de50c7089
Fabian Röling <fabianroeling@googlemail.com>
2022-01-23 03:39:35 +0100

    Update ClaimsData.js

--- ClaimsData.js
+++ ClaimsData.js
@@ -114,4 +114,5 @@ drawClaim(1700,3250,1800,3350,"Yumca's Purple fuzzball",186,"Yumca");
 drawClaim(837,4976,933,5062,"Wandering Trader Kidnapping Platform",187,"Yumca");
 drawClaim(1550,11800,1650,11900,"Yumca's 1.17 Mining Base",188,"Yumca");
 drawClaim(11083,409,12406,1536,"Mining desert",190,"FaRo1");
+drawClaim(-1000,-3200,-600,-2700,"LightningAussie",192,"LightningAussie");
 }

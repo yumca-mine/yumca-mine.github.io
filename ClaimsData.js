ee2dd7a19e42cd33a641e9bf636851dc6cac6f05
Fabian Röling <fabianroeling@googlemail.com>
2022-08-17 15:18:25 +0200

    Update ClaimsData.js

--- ClaimsData.js
+++ ClaimsData.js
@@ -120,8 +120,8 @@ drawClaim(320,-1985,703,-1729,"FaRo's base",195,"FaRo1");
 drawClaim(-10200,2000,-9890,2400,"Deltaflight\’s Base",196,"Deltaflight");
 drawClaim(-9598,3487,-9425,3747,"MarkoTrinpa's cave base",197,"Markotrinpa");
 drawClaim(-2580,3445,-3034,3163,"Sunset City", 198,"jazz_box");
-//drawClaim(13561,-8912,16374,-8822,"JochCool's Outpost",199,"JochCool");
 drawClaim(1764,-3234,1849,-3176,"vegguid's Raid farm",200,"vegguid");
 drawClaim(1093,3928,1273,4055,"seallz' village",202,"seallz");
 drawClaim(1328,1328,1343,1343,"MetalManiacMc's Chunk",203,"MetalManiacMc");
+drawClaim(16375,-8911,16531,-8826,"JochCool's Outpost",204,"JochCool");
 }

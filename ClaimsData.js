5588fabec75d911731e608a6253fddfc86f172c6
Fabian Röling <fabianroeling@googlemail.com>
2023-01-18 10:10:35 +0100

    Update ClaimsData.js

--- ClaimsData.js
+++ ClaimsData.js
@@ -130,4 +130,5 @@ drawClaim(-980,300,-813,467,"Find Yourself",211,"Yumca");
 drawClaim(1472,3008,1855,3391,"YumOctMapZone",212,"Yumca");
 drawClaim(10400,-5920,10559,-5777,"Public Pillager Farm",213,"JurassicBeast");
 drawClaim(62,-2382,110,-2341,"Thomas' Temple",214,"ThomasToSpace");
+drawClaim(-3722,-423,-3599,-284,"SportyPompano's claim",216,"SportyPompano");
 }
